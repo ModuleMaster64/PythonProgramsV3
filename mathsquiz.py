f1730a0b385e9622215e689a1caa9c9893bb1230
M0Dul2Mast2r64 <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-03 13:19:31 +0100

    Update and rename mathproblems.py to mathsquiz.py

---
+++ mathsquiz.py
@@ -0,0 +1,31 @@
+# 🧠 Fun Interactive Maths Quiz
+# Included round for decimals!
+
+# 🦛 Hippo Problem
+print("If a 2000 pound pregnant hippo gives birth to a 100 pound calf,")
+print("but then eats 50 pounds of food, how much does she weigh?")
+input("Press the enter key to find out.")
+print("2000 - 100 + 50 =", 2000 - 100 + 50)
+print()
+
+# ⏳ Time Conversion
+print("If an adventurer returns from a successful quest and rests for 3 hours,")
+print("how much time is that in minutes?")
+input("Press the enter key to find out.")
+hours = 3
+minutes = hours * 60
+print(f"{hours} hours is equal to {minutes} minutes.")
+print()
+
+# 💰 Splitting the Bill
+print("Your friends split £19 evenly 4 ways, how much do you each throw in?")
+input("Press enter key to find out.")
+print("19 / 4 =", round(19 / 4, 2))
+print()
+
+print("If a restaurant check comes to £25 with tip and you and")
+print("your friend split it 2 ways, how much do you throw in?")
+input("Press enter key to find out.")
+print("25 / 2 =", round(25 / 2, 2))
+
+input("\n\nPress the enter key to exit the .py terminal")
