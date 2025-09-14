741369dc2a4f550675164d7b34837e4bf1030577
M0Dul2Mast2r64 <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-14 21:42:56 +0100

    Update and rename Usrname.py to Username.py
    added a complementary 'please'
    
    please please please (Sabrina Carpenter reference) LOL

--- system/Username.py
+++ system/Username.py
@@ -4,8 +4,8 @@ def user_name(forename, last_name, year):
     print("Your user name is " + username_out)
 
 
-    first_name = input("Enter your first name: ")
-    surname = input("Enter your surname: ")
-    joined = input("Enter the year you were born: ")
+    first_name = input("Please Enter your first name: ")
+    surname = input("Please Enter your surname: ")
+    joined = input("Please Enter the year you were born: ")
 
     user_name(first_name,surname,joined)
