7b06b480b77788ac95d604537cf7a0d6613fec43
TheM1ddleM1n <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-20 13:24:45 +0100

    Update Story.py

--- Other Programs/Story.py
+++ Other Programs/Story.py
@@ -1,9 +1,9 @@
 # String data types
 
-# Subroutine to process a phrase
+# Subroutine to process a phrase!
 def Output(Phrase):
     print(Phrase[20:26])
 
 # Main program
-Phrase = "I saw a wolf in the Forest. A lonely wolf."
+Phrase = "I saw a wolf in the forest. A lonely wolf."
 Output(Phrase)
