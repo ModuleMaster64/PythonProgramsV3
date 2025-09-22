a5f5ad1c0c02f88b84137b0fff058325119c2154
TheM1ddleM1n <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-22 21:53:17 +0100

    Update mathsquiz.py

--- Number/mathsquiz.py
+++ Number/mathsquiz.py
@@ -8,7 +8,7 @@ input("Press the enter key to find out.")
 print("2000 - 100 + 50 =", 2000 - 100 + 50)
 print()
 
-# ⏳ Time Conversion
+# ⏳ Time Conversion question
 print("If an adventurer returns from a successful quest and rests for 3 hours,")
 print("how much time is that in minutes?")
 input("Press the enter key to find out.")
@@ -17,15 +17,16 @@ minutes = hours * 60
 print(f"{hours} hours is equal to {minutes} minutes.")
 print()
 
-# 💰 Splitting the Bill
+# 💰 Splitting the Bill question
 print("Your friends split £19 evenly 4 ways, how much do you each throw in?")
 input("Press enter key to find out.")
 print("19 / 4 =", round(19 / 4, 2))
 print()
 
+# Another Bill Question
 print("If a restaurant check comes to £25 with tip and you and")
 print("your friend split it 2 ways, how much do you throw in?")
 input("Press enter key to find out.")
 print("25 / 2 =", round(25 / 2, 2))
 
-input("\n\nPress the enter key to exit the .py terminal")
+input("\n\nPress the enter key to exit the mathquiz .py terminal!")
