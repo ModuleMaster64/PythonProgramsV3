ea25174a0073ebb77f30bf8d89fc35098b2d7efc
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-10-07 17:30:34 +0100

    Update Security.py

--- Other Programs/Security.py
+++ Other Programs/Security.py
@@ -1,12 +1,12 @@
-print('Welcome to System Security Inc.')
+print('Greetings! Welcome to System Security Inc.')
 print('-- where security is our middle name\n')
 
 password = input('Enter your password: ')
 
 if password == "n0tY0u2av5r1gepy3th0nc0de264..?!":
-    print('Access Granted')
+    print('Access has been Granted!')
 else:
-        print('Access Denied')
+        print('Access has been Denied! Get out of here!')
 
 input('\n\nPress the enter key to exit.')
 
