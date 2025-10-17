defcd6b61d90667fdc44b574558248173c21f48b
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-10-17 21:33:09 +0100

    Update S&L1Player.py
    gg

--- Number/S&L1Player.py
+++ Number/S&L1Player.py
@@ -1,40 +1,47 @@
+"""Snakes and Ladders Game - Single Player!"""
 import random
-Squares = 0
+
+current_square = 0
 print('Welcome to the Snakes and Ladders The GAME!')
-print('You must reach 100 to win the game')
-while Squares < 101:
-    print('Roll the dice by pressing enter')
+print('You must reach sqaure 100 to win this game.')
+
+while current_square < 101:
+    print('Roll the dice by pressing enter.')
     input()
-    DiceRoll = random.randint(1,6)
-    print('You rolled a',DiceRoll)
-    Squares = Squares + DiceRoll
-    Squares = str(Squares)
-    print('You are on Square' + Squares)
-    Squares = int(Squares)
-    if Squares == 8:
+    dice_roll = random.randint(1, 6)
+    print(f'You rolled a {dice_roll}')
+    current_square += dice_roll
+    print(f'You are on square {current_square}')
+    
+    # Ladders
+    if current_square == 8:
         print('Well done, go up the ladder to square 23')
-        Squares = Squares + 15
-    if Squares ==10:
-        print('Oh no, you have landed on a snake,go back five')
-        Squares = Squares - 5
-    if Squares ==54:
+        current_square += 15
+    elif current_square == 54:
         print('Well done, go up the ladder to square 60')
-        Squares = Squares + 60
-    if Squares ==15:
-        print('Oh no, you have landed on a snake, go down 5 squares')
-        Squares = Squares - 5
-    if Squares ==24:
+        current_square = 60  # refractored: was adding 60 instead of setting to 60
+    elif current_square == 24:
         print('Well done, go up the ladder to square 29')
-        Squares = Squares + 5
-    if Squares ==30:
-        print('Oh no, you have landed on a snake go down 8 spaces')
-        Squares = Squares - 8
-    if Squares ==21:
+        current_square += 5
+    elif current_square == 21:
         print('Well done, go up the ladder to square 23')
-        Squares = Squares + 2
-    if Squares ==31:
+        current_square += 2
+    
+    # Snakes
+    if current_square == 10:
+        print('Oh no, you have landed on a snake, go back five')
+        current_square -= 5
+    elif current_square == 15:
+        print('Oh no, you have landed on a snake, go down 5 squares')
+        current_square -= 5
+    elif current_square == 30:
+        print('Oh no, you have landed on a snake go down 8 spaces')
+        current_square -= 8
+    elif current_square == 31:
         print('Oh no, you have landed on a snake, go down five spaces')
-        Squares = Squares - 5
-    if Squares ==63:
+        current_square -= 5
+    elif current_square == 63:
         print('Oh no, you have landed on the biggest snake')
-        Squares = Squares - 60
+        current_square -= 60
+
+print('🎉 Congratulations! You won!')
